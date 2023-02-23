6b2192861a08a76d8981f221c0d43c22248c73be
Fabfr0st <fabienlastere@spyrals.fr>
2023-02-23 16:31:27 +0100

    wip

--- src/site/main/service.tsx
+++ src/site/main/service.tsx
@@ -3,21 +3,21 @@ import ButtonLink from '../ui/ButtonLink'
 import ServiceValue from './service-value'
 
 export default () =>
-  <section className="flex flex-row items-center justify-between" id='offre'>
+  <section className="flex flex-row mx-24" id='offre'>
     <ServiceLeft />
     <ServiceRight />
   </section>
 
 const ServiceLeft = () =>
-  <div className="flex flex-col">
+  <div className="flex flex-col items-center justify-between mx-4">
     <ServiceValue className='bg-violet-400' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reiciendis nisi sapiente iste nostrum dicta, rem ratione iusto in et repellendus voluptate ipsum id incidunt sunt eveniet vero ipsam ea.' title="Service / Valeur" />
     <ServiceValue className='bg-purple-500' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reiciendis nisi sapiente iste nostrum dicta, rem ratione iusto in et repellendus voluptate ipsum id incidunt sunt eveniet vero ipsam ea.' title="Service / Valeur" />
     <ServiceValue className='bg-purple-600' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reiciendis nisi sapiente iste nostrum dicta, rem ratione iusto in et repellendus voluptate ipsum id incidunt sunt eveniet vero ipsam ea.' title="Service / Valeur" />
   </div>
 
 const ServiceRight = () =>
-  <div className="flex flex-col items-center justify-between">
+  <div className="flex flex-col items-center justify-between mx-4">
     <h1 className="text-8xl">NOTRE OFFRE</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reiciendis nisi sapiente iste nostrum dicta, rem ratione iusto in et repellendus voluptate ipsum id incidunt sunt eveniet vero ipsam ea.</p>
+    <p className="mx-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores reiciendis nisi sapiente iste nostrum dicta, rem ratione iusto in et repellendus voluptate ipsum id incidunt sunt eveniet vero ipsam ea.</p>
     <ButtonLink className="ml-80" href="contact" label="Nous contacter" />
   </div>
