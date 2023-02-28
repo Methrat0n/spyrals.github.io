2d3e925cad7ad16f2e4030da1a9c69b7e7426996
Fabfr0st <fabienlastere@spyrals.fr>
2023-02-28 14:20:13 +0100

    fix(header): links redirect to url

--- src/ui/atoms/Anchor.tsx
+++ src/ui/atoms/Anchor.tsx
@@ -6,6 +6,6 @@ type Props = {
 }
 
 export default ({href, label}: Props) =>
-  <a className="hover:underline decoration-yellow underline-offset-4 decoration-2" href={`#${href}`}>
+  <a className="hover:underline decoration-yellow underline-offset-4 decoration-2" href={`https://www.spyrals.fr/#${href}`}>
     <h3>{label}</h3>
   </a>
