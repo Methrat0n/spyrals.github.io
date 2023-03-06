20937c5f899b041076dffed387597b63e8c87647
Fabfr0st <fabienlastere@spyrals.fr>
2023-03-06 16:47:50 +0100

    feat(header): responsive

--- src/ui/organism/header/navigation-and-social.tsx
+++ src/ui/organism/header/navigation-and-social.tsx
@@ -4,13 +4,14 @@ import config from '../../../utils/config'
 import SocialNetwork from '../../atoms/SocialNetwork'
 
 export default () =>
-  <div className="flex justify-between w-5/12 h-full items-center">
+  <div className="flex flex-row-reverse sm:flex-row justify-between w-5/12 h-full items-center">
     <Navigation />
     <Socials />
+    <FixedMobile />
   </div>
 
 const Navigation = () =>
-  <div className="flex w-full justify-evenly items-center">
+  <div className="hidden sm:flex w-full justify-evenly items-center">
     <Anchor href={config.menu.link_1} label={config.menu.label_1} />
     <Anchor href={config.menu.link_2} label={config.menu.label_2} />
     <Anchor href={config.menu.link_3} label={config.menu.label_3} />
@@ -20,7 +21,20 @@ const Navigation = () =>
 const socialsStyles = "mx-1 h-7 py-2 bg-purple-spyrals octagon w-7"
 
 const Socials = () =>
-  <div className="flex flex-row">
+  <div className="hidden sm:flex flex-row">
     <SocialNetwork className={`${socialsStyles}`} logo={config.svg_path + config.socials.facebook_logo_white} href={config.socials.facebook_link} />
     <SocialNetwork className={`${socialsStyles}`} logo={config.svg_path + config.socials.linkedin_logo_white} href={config.socials.linkedin_link} />
+  </div>
+
+const fixedBarStyle = "flex flex-col p-2"
+const imagesStyle = "w-7 h-7"
+
+const FixedMobile = () =>
+  <div className='sm:hidden'>
+    <a className={`rounded-l-md bg-redish ${fixedBarStyle}`} href={config.fixed.contact_link}>
+      <img className={imagesStyle} src={config.svg_path + config.fixed.contact_img}/>
+    </a>
+    <a className={`rounded-l-md bg-orange ${fixedBarStyle}`} href={config.fixed.video_link}>
+      <img className={imagesStyle} src={config.svg_path + config.fixed.video_img}/>
+    </a>
   </div>
