aac79ef13ac856290919311b52e80d804ebd853b
Fabfr0st <fabienlastere@spyrals.fr>
2023-03-07 12:20:28 +0100

    feat(site): description & icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import config from './utils/config'
 import Footer from './ui/organism/footer'
 import Header from './ui/organism/header'
 
@@ -11,6 +12,9 @@ export default ({ Main }: Props) =>
   <head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content={config.home_description} />
+    <title>{config.home_title}</title>
+    <link rel="icon" href="/spyrals.svg" type="image/svg+xml" />
     <link href="/output.css" rel="stylesheet" />
   </head>
   <body className="bg-purple-void">
