d9e55fb74997c4f891e15f6c8efaed04ccc9af0f
Fabfr0st <fabienlastere@spyrals.fr>
2023-03-08 13:16:24 +0100

    fix(worklife): gallery stays in body

--- src/pages/home/worklife.tsx
+++ src/pages/home/worklife.tsx
@@ -3,14 +3,14 @@ import config from '../../utils/config'
 import BordedPicture from '../../ui/atoms/BordedPicture'
 
 export default () =>
-  <section className="flex flex-col items-center pt-24 sm:ml-32" id='worklife'>
+  <section className="flex flex-col items-center pt-24 sm:ml-32 overflow-x-hidden" id='worklife'>
     <h1 className="text-4xl sm:text-6xl self-start ml-16 font-title font font-semibold max-sm:mx-5 max-sm:self-center">{config.worklife.title}</h1>
     <p className="my-12 sm:mx-96 mx-5 max-sm:text-justify">{config.worklife.text}</p>
     <Gallery />
   </section>
 
 const Gallery = () =>
-  <div className="flex flex-row overflow-auto py-20 pl-4 hide-scroll">
+  <div className="flex flex-row overflow-auto py-20 pl-28 sm:pl-4 hide-scroll">
     {config.worklife.gallery.map((frame, index) => {
       return <Frame 
       img={frame.img} 
