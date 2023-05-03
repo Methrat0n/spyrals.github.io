bc6915f98b7881d203bd4e8d086f1fa845295c8f
Fabfr0st <fabienlastere@spyrals.fr>
2023-05-03 16:24:33 +0200

    fix(worklife): reduced margin bottom text

--- src/pages/home/worklife.tsx
+++ src/pages/home/worklife.tsx
@@ -5,12 +5,12 @@ import BordedPicture from '../../ui/atoms/BordedPicture'
 export default () =>
   <section className="flex flex-col items-center pt-24 md:ml-10 lg:ml-32 overflow-x-hidden" id='worklife'>
     <h1 className="text-4xl md:text-5xl lg:text-6xl self-start lg:ml-16 font-title font font-semibold max-md:mx-5 max-md:self-center">{config.worklife.title}</h1>
-    <p className="my-12 md:mx-14 lg:mx-96 mx-5 max-md:text-justify">{config.worklife.text}</p>
+    <p className="mt-12 mb-6 md:mx-14 lg:mx-96 mx-5 max-md:text-justify">{config.worklife.text}</p>
     <Gallery />
   </section>
 
 const Gallery = () =>
-  <div className="flex flex-row overflow-auto py-20 pl-28 md:pl-4 hide-scroll w-screen md:w-full">
+  <div className="flex flex-row overflow-auto pt-[2rem] pb-20 md:py-20 pl-28 md:pl-4 hide-scroll w-screen md:w-full">
     {config.worklife.gallery.map((frame, index) =>
       <Frame 
         img={frame.img} 
