c0b885eddcd0575b8225cbf3f3f6acf617924b26
Fabfr0st <fabienlastere@spyrals.fr>
2023-05-25 10:46:22 +0200

    fix(offer): overflow hidden

--- src/pages/home/service.tsx
+++ src/pages/home/service.tsx
@@ -10,7 +10,7 @@ export default () =>
   </section>
 
 const ServiceLeft = () =>
-  <div className="flex flex-col items-center justify-center w-full md:w-1/2 md:mr-12 md:max-w-md">
+  <div className="flex flex-col items-center justify-center w-full md:w-1/2 md:mr-12 md:max-w-md overflow-hidden">
     <ServiceValue className='bg-purple-spyrals self-start rotate-2 md:rotate-0 -ml-4 md:ml-0 rounded-r-xl' id='offerBloc1' text={config.offer.card1_text} title={config.offer.card1_title} />
     <ServiceValue className='bg-purple-dark self-end -rotate-2 md:rotate-0 -mr-4 md:mr-0 rounded-l-xl' id='offerBloc2' text={config.offer.card2_text} title={config.offer.card2_title} />
     <ServiceValue className='bg-purple-darkest self-start rotate-6 md:rotate-0 -mr-4 md:mr-0 rounded-r-xl' id='offerBloc3' text={config.offer.card3_text} title={config.offer.card3_title} />
