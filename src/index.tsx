ffec45d9ae79762b45559a217372563b0ecc19ae
User <user@MacBook-Pro-de-User.local>
2023-09-07 11:45:07 +0200

    feat Google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -11,24 +11,27 @@ type Props = {
   preloads?: ReadonlyArray<Preload>
 }
 
-export default ({ Main, description, title, preloads = [] }: Props) =>
-<html className='hide'>
-  <head>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content={description} />
-    <title>{title}</title>
-    <link rel="icon" href="/spyrals.svg" type="image/svg+xml" />
-    <link href="/output.css" rel="stylesheet" />
-    {
-      preloads.map((preload) => <link rel="preload" {...preload} fetchpriority="high" />)
-    }
-    <script defer src="./assets/js/team.js"></script>
-    <script defer src="./assets/js/video.js"></script>
-  </head>
-  <body className="bg-purple-void">
-    <Header />
-    <Main />
-    <Footer />
-  </body>
-</html>
+export default ({ Main, description, title, preloads = [] }: Props) => {
+  return (
+    <html className='hide'>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <title>{title}</title>
+        <link rel="icon" href="/spyrals.svg" type="image/svg+xml" />
+        <link href="/output.css" rel="stylesheet" />
+        {
+          preloads.map((preload) => <link rel="preload" {...preload} fetchpriority="high" />)
+        }
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7SZWQT723Y"></script>
+        <script src="./assets/js/gaIndex.js"></script>
+      </head>
+      <body className="bg-purple-void">
+        <Header />
+        <Main />
+        <Footer />
+      </body>
+    </html>
+  )
+}
