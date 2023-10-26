566bf1644b43c956529f7bbe5c1892316359a50f
thibautwormkil <thibautjouen@spyrals.fr>
2023-10-26 14:41:18 +0200

    Added small animation after failed to implement better one with react-animation-on-scroll

--- src/ui/atoms/QuoteContent.tsx
+++ src/ui/atoms/QuoteContent.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
 type Props = {
+  animDelay: string // Used to have them launch there animation with a small time gap
   text: string
   author: string
 }
 
-export default ({text, author}: Props) =>
+export default ({animDelay, text, author}: Props) =>
   <div className="relative flex flex-col">
     <div className="flex flex-row">
-      <img className="" src='/images/node_leftBar.webp' fetchpriority='low'/>
+      <img className={"animate-pulse animate-infinite animate-ease-linear animate-duration-[3000ms] "+animDelay} src='/images/node_leftBar.webp' fetchpriority='low'/>
       <p className="ml-4 mt-2">{text}</p>
     </div>
 
     <div className="flex justify-between relative">
-      <img className="absolute -top-4 object-contain" src='/images/quote_node.webp' fetchpriority='low'/>
+      <img className={"absolute -top-4 object-contain animate-pulse animate-infinite animate-ease-linear animate-duration-[3000ms] "+animDelay} src='/images/quote_node.webp' fetchpriority='low'/>
       <p className="absolute top-8 right-0 font-title font-semibold text-yellow h-12">{author}</p>
     </div>
 
